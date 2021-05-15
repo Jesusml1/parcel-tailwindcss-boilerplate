733d31af0223b0409510564ee29bfbc688a53175
jesus <jesus_lopez1997@hotmail.com>
2021-05-15 19:12:39 -0400

    Add tailwind and postcss configuration

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
-console.log("hello there");
+import "./styles/index.css";
+const obj = {
+  a: 1,
+  b: 2,
+  c: {
+    cd: 3,
+  },
+};
+
+console.log(obj?.c?.cd, obj?.f);
 
 if (module.hot) {
   module.hot.accept();
